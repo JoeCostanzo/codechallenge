6d7bf50ef2d4209b05f8afe3c9c3c009bf5b4a1f
joeco <joe@joeco.info>
2017-03-31 02:33:04 -0400

    Files properly export their methods as es6 modules. JSdocs updated.

--- src/pascalsTriangle.js
+++ src/pascalsTriangle.js
@@ -1,5 +1,5 @@
 'use strict';
-let numTiers = 30,
+let numTiers = 10,
   executionTime,
   triangle,
   start,
@@ -7,53 +7,65 @@ let numTiers = 30,
   prior = '',
   buf = [];
 
-/**
- * Finds n-tiers of Pascal's Triangle, without using looping.
- *
- * @function
- * @param {Number} number - The desired number of tiers.
- * @param {Array} array - An initializer, representing the first tier of the triangle.
- * @returns {Array}
- */
-function pascalOnlyRecursive(number, array = [[1]]) {
-  if (number < 2) return array; // first tier already exists
+const pt = {
+  /**
+   * Finds n-tiers of Pascal's Triangle, without using looping.
+   *
+   * @function
+   * @param {Number} number - The desired number of tiers.
+   * @param {Array} array - An initializer, representing the first tier of the triangle.
+   * @returns {Array}
+   */
+  pascalOnlyRecursive(number, array = [[1]]) {
+    if (number < 2) return array; // first tier already exists
 
-  const findCurTier = ({
-    curTier = [1],
-    prevTier = array[array.length - 1],
-    i = 1
-  }) => {
-    if (i == prevTier.length) return curTier;
-    curTier[i] = prevTier[i] + prevTier[i - 1];
-    return findCurTier({ curTier, i: i + 1 });
-  };
+    const findCurTier = ({
+      curTier = [1],
+      prevTier = array[array.length - 1],
+      i = 1
+    }) => {
+      if (i == prevTier.length) return curTier;
+      curTier[i] = prevTier[i] + prevTier[i - 1];
+      return findCurTier({ curTier, i: i + 1 });
+    };
 
-  let curTier = findCurTier({});
+    let curTier = findCurTier({});
 
-  curTier.push(1);
-  array.push(curTier);
+    curTier.push(1);
+    array.push(curTier);
 
-  return pascalOnlyRecursive(number - 1, array);
-}
+    return pt.pascalOnlyRecursive(number - 1, array);
+  },
 
-start = new Date().getMilliseconds();
-triangle = pascalOnlyRecursive(numTiers).reverse();
-stop = new Date().getMilliseconds();
-executionTime = stop - start;
+  /**
+   *
+   * @return {String}
+   * @constructor
+   */
+  VerifyPascalOnlyRecursive() {
+    start = new Date().getMilliseconds();
+    triangle = pt.pascalOnlyRecursive(numTiers).reverse();
+    stop = new Date().getMilliseconds();
+    executionTime = stop - start;
 
 
-for (let tier of triangle) {
-  let newLine = tier.join(" ");
-  let diff = prior.length - newLine.length;
-  if (diff > 0) {
-    newLine = `${' '.repeat(diff/2)}${newLine}${' '.repeat(diff/2)}`;
+    for (let tier of triangle) {
+      let newLine = tier.join(" ");
+      let diff = prior.length - newLine.length;
+      if (diff > 0) {
+        newLine = `${' '.repeat(diff/2)}${newLine}${' '.repeat(diff/2)}`;
+      }
+      buf.push(newLine);
+      prior = newLine;
+    }
+    console.log(buf.reverse().join("\n"));
+    return `Pascal's Triangle done. Execution time: ${executionTime}\n`;
+
   }
-  buf.push(newLine);
-  prior = newLine;
-}
-console.log(buf.reverse().join("\n"));
-console.log(`Execution time: ${executionTime}\n`);
 
+};
+
+export default pt;
 /**
  * Created by joec on 2/2/2017.
  */
--- src/pi.js
+++ src/pi.js
@@ -1,41 +1,52 @@
 'use strict';
-let executionTime,
-  add_sub_bool,
-  pi,
-  start,
-  stop;
 
-/**
- * Calculates pi using Leibniz's formula.
- *
- * [In lieu of an infinite series,
- * takes avg of two passes 8 digits in length,
- * to find the closest approx.]
- * Correct to 15 decimal places.
- *
- * @returns {number}
- */
-function calculatePi(passes = [25554500, 25554502]) {
-  return passes.map(n => {
-    add_sub_bool = true;
-    return Array(n).fill().reduce((a, _, i) => {
-      if (i % 2 !== 0) {
-        if (add_sub_bool = !add_sub_bool) {
-          return a - 1 / i;
+const pi = {
+  /**
+   * Calculates pi using Leibniz's formula.
+   *
+   * [In lieu of an infinite series,
+   * takes avg of two passes 8 digits in length,
+   * to find the closest approx.]
+   * Correct to 15 decimal places.
+   *
+   * @returns {Number}
+   */
+  calculatePi(passes = [25554500, 25554502]) {
+    let add_sub_bool;
+    return passes.map(n => {
+      add_sub_bool = true;
+      return Array(n).fill().reduce((a, _, i) => {
+        if (i % 2 !== 0) {
+          if (add_sub_bool = !add_sub_bool) {
+            return a - 1 / i;
+          }
+          return a + 1 / i;
         }
-        return a + 1 / i;
-      }
-      return a;
-    }, 0) * 4;
-  }).reduce((a, b) => a + b, 0) / 2;
-}
+        return a;
+      }, 0) * 4;
+    }).reduce((a, b) => a + b, 0) / 2;
+  },
 
-start = new Date().getMilliseconds();
-pi = calculatePi();
-stop = new Date().getMilliseconds();
-executionTime = stop - start;
-console.log(pi); //3.141592653589793
-console.log(`Execution time: ${executionTime}\n`);
+
+  /**
+   *
+   * @return {String}
+   * @constructor
+   */
+  VerifyPi() {
+    let mPi, executionTime, start, stop;
+    console.log(`Pi calculation starting. This could take a while...`);
+    start = new Date().getMilliseconds();
+    mPi = pi.calculatePi();
+    stop = new Date().getMilliseconds();
+    executionTime = stop - start;
+    console.log(mPi); //3.141592653589793
+    return `Pi found. Execution time: ${executionTime}\n`;
+  }
+
+};
+
+export default pi;
 
 /**
  * Created by joec on 2/13/2017.
