7b9e15a1c3f9089906a26e06a9c68e8651782c6a
joeco <joe@joeco.info>
2017-03-31 02:33:48 -0400

    test consumes index, and verifies all 3 current modules.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
-import katas from '../src/katas';
+import * as challenge from '../index';
 
-console.log(katas.VerifyAllKatas());
+console.log(challenge.katas.VerifyAllKatas());
+console.log(challenge.pt.VerifyPascalOnlyRecursive());
+console.log(challenge.pi.VerifyPi());
 
 /**
  * Created by joec on 3/30/2017.
