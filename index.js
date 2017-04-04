bd6497b1688164ffc3aefee68d6a0350f7e835e1
joeco <joe@joeco.info>
2017-04-04 09:36:08 -0400

    exports jsast needed for some npm applications (see react-module-loader)

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import katas from './src/katas';
 import pt from './src/pascalsTriangle';
 import pi from './src/pi';
+import jsast from './src/jsast/jsdoc-ast.json';
 
-export { katas, pt, pi };
+export { katas, pt, pi, jsast };
 /**
  * Created by joec on 3/31/2017.
  */
