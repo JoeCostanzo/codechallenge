b68fe15fa92616253f526a7d8e0c64d4f87926b4
joeco <joe@joeco.info>
2017-04-08 13:18:36 -0400

    index moved to src/ & updated to reflect new jsast location

--- index.js
+++
@@ -1,9 +0,0 @@
-import katas from './src/katas';
-import pt from './src/pascalsTriangle';
-import pi from './src/pi';
-import jsast from './src/jsast/jsdoc-ast.json';
-
-export { katas, pt, pi, jsast };
-/**
- * Created by joec on 3/31/2017.
- */
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import katas from './katas';
+import pt from './pascalsTriangle';
+import pi from './pi';
+import jsast from '../dist/jsast/jsdoc-ast.json';
+
+export { katas, pt, pi, jsast };
+/**
+ * Created by joec on 3/31/2017.
+ */
